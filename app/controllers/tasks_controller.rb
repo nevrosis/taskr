0870cdc5b2b14ccfc04dbecce53a1c95ffcc8c69
David Paquette <paquettedavid@me.com>
2017-05-19 11:20:33 -0400

    Auth user + Task Form and Partials

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,59 @@
+class TasksController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_task, only:[:edit, :update, :show, :destroy]
+
+  def index
+
+    @tasks = current_user.tasks
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(tasks_params)
+    if @task.save
+      flash[:notice] = "Task was created"
+      redirect_to task_path(@task)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(tasks_params)
+      flash[:notice] = "Task was updated"
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:notice] = "Task deleted"
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+
+end
---
+++ app/models/task.rb
@@ -0,0 +1,8 @@
+class Task < ApplicationRecord
+
+
+  validates :content, presence: true, length: {minimum: 4, maximum: 300}
+
+  belongs_to :user
+
+end
